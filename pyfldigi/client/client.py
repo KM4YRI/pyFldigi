1ad54346e06dd3122ff3e394c264c320403aa109
KM4YRI <km4yri@gmail.com>
2017-02-16 00:40:49 -0500

    make the xml-rpc client use the new requests transport class.  Added some documentation.  Added a default console and logger that can be optionally enabled.  started adding some logging calls, will add more as I get a chance to.

--- pyfldigi/client/client.py
+++ pyfldigi/client/client.py
@@ -6,6 +6,7 @@ import time
 import logging
 import threading
 import xmlrpc.client
+from .transport import RequestsTransport
 from .txmonitor import TxMonitor
 from .main import Main
 from .modem import Modem
@@ -23,6 +24,29 @@ class Client(object):
 
     `Official FLDIGI XML-RPC Protocol Documentation page <http://www.w1hkj.com/FldigiHelp-3.21/html/xmlrpc_control_page.html/>`_
 
+    :param hostname: IP address of the xml-rpc FLDIGI interface (usually 127.0.0.1)
+    :type hostname: str
+
+    :param port: The port of the xml-rpc FLDIGI interfac (usually 7362)
+    :type port: int
+
+    :Example:
+
+    >>> import pyfldigi
+    >>> fldigi = pyfldigi.Client(hostname='127.0.0.1', port=7362)  # <-- Constructor call
+    >>> fldigi.name
+    'fldigi'
+
+
+    .. note::
+        This class spawns a TX Monitor thread that ensures the transmitter gets keyed on and off at the appropriate
+        times.  It monitors Transmit duty cycle (%), max transmit time (seconds) aka Dead Man's Switch?, and max text
+        length.  You can set these with getters and setters (TBD), but you should probably just leave them alone.
+
+        You get all of this functionality for free.  I don't see any use-case where it wouldn't be useful, so it can't
+        be disabled.  It uses a daemon thread which starts when Client() is instantiated (which calls Text()), and
+        ends when the program shuts down.
+
     .. note::
         Instances of the following classes will be instantiated and added as properties to the Client() object:
 
@@ -39,29 +63,12 @@ class Client(object):
         the XML-RPC function names.  I've taken a bit of artistic liberty with naming and grouping!
     '''
 
-    def __init__(self, hostname='127.0.0.1', port=7362, reset=True):
-        '''Client() constructor
-
-        :param hostname: IP address of the xml-rpc FLDIGI interface (usually 127.0.0.1)
-        :type hostname: str
-
-        :param port: The port of the xml-rpc FLDIGI interfac (usually 7362)
-        :type port: int
-
-        :Example:
-
-        >>> import pyfldigi
-        >>> fldigi = pyfldigi.Client(hostname='127.0.0.1', port=7362)  # <-- Constructor call
-        >>> fldigi.name
-        'fldigi'
-        '''
+    def __init__(self, hostname='127.0.0.1', port=7362, reset=True, log=False):
         self.logger = logging.getLogger('pyfldigi.Client')
         self.ip_address = hostname
         self.port = port
         self.logger.debug('Attempting to connect to connect to fldigi at IP address={}, port={}, via XMP-RPC'.format(self.ip_address, self.port))
-        self.client = xmlrpc.client.ServerProxy('http://{}:{}/'.format(self.ip_address, self.port), use_builtin_types=True, allow_none=True)
-        self.mutex = threading.Lock()  # Mutex for xml-rpc client.  Guard all calls with this.
-        self.txmonitor = TxMonitor(clientObj=self)
+        self.client = xmlrpc.client.ServerProxy('http://{}:{}/'.format(self.ip_address, self.port), transport=RequestsTransport(use_builtin_types=True), allow_none=True)
         self.main = Main(clientObj=self)
         self.modem = Modem(clientObj=self)
         self.rig = Rig(clientObj=self)
@@ -70,6 +77,24 @@ class Client(object):
         self.spot = Spot(clientObj=self)
         self.flmsg = Flmsg(clientObj=self)
         self.io = Io(clientObj=self)
+        self.txmonitor = TxMonitor(clientObj=self)  # do this last
+
+    def startLogger(self, level=logging.INFO, filename=None):
+        '''Call this method if you don't have a dedicated logger in your python script.'''
+        self.logger.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s : pyfldigi.client : %(message)s')
+        self.sh = logging.StreamHandler()
+        self.sh.setLevel(level)
+        self.sh.setFormatter(formatter)
+        self.logger.addHandler(self.sh)
+
+        if filename is not None:
+            self.fh = logging.FileHandler(filename)
+            self.fh.setLevel(level)
+            self.fh.setFormatter(formatter)
+            self.logger.addHandler(self.fh)
+
+        self.logger.info('startLogger() : Logging has been enabled')
 
     @property
     def methods(self):
@@ -101,7 +126,8 @@ class Client(object):
         >>> fldigi.name
         fldigi
         '''
-        return self.client.fldigi.name()
+        name = self.client.fldigi.name()
+        self.logger.debug('name returned {}'.format(name))
 
     @property
     def version(self):
